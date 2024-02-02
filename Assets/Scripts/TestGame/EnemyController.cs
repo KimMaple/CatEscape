572bc49ba407db6999f1c563ae44c5e3a44ee916
DESKTOP-FIGQE68\k3421 <k3421533@gmail.com>
2024-02-02 12:05:30 +0900

    유니티 교과서

--- Assets/Scripts/TestGame/EnemyController.cs
+++ Assets/Scripts/TestGame/EnemyController.cs
@@ -6,26 +6,43 @@ public class EnemyController : MonoBehaviour
 {
     [SerializeField] Animator capAnim;
 
-    private enum State{
+    private enum State
+    {
         Idle, Hit, HitDead, DeadGround
     }
+    private State state = State.Idle;
 
     public int capHp = 2;
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        State state = State.Idle;
-        if(Input.GetMouseButtonDown(0))
+        if (capHp > 0)
+        {
+            state = State.Idle;
+            this.capAnim.SetInteger("CaptainState", (int)state);
+        }
+
+        if (Input.GetMouseButtonDown(0))
         {
             state = State.Hit;
             capHp -= 1;
+            if(capHp == 0)
+            {
+                state = State.HitDead;
+            }
+            this.capAnim.SetInteger("CaptainState", (int)state);
         }
-        this.capAnim.SetInteger("CaptainState", (int)state);
+        
+        Debug.Log(capHp);
 
+        if (capHp <= 0)
+        {
+            state = State.DeadGround;
+            this.capAnim.SetInteger("CaptainState", (int)state);
+        }
     }
 }
